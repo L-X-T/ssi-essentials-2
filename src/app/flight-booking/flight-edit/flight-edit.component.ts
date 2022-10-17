7e6cfca7b2a0172bda4a66a4c6f7ac4540b8e99a
Alex Thalhammer <alex@angular-software.at>
2022-10-17 11:37:13 +0200

    [RF] Change updateOn to 'blur'

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -16,10 +16,24 @@ export class FlightEditComponent implements OnChanges, OnInit, OnDestroy {
   @Input() flight: Flight | undefined | null;
 
   editForm: FormGroup = this.fb.group({
-    id: [0, Validators.required],
-    from: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
-    to: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
-    date: ['', [Validators.required, Validators.minLength(33), Validators.maxLength(33)]]
+    id: [0, Validators.required, []],
+    from: [
+      '',
+      {
+        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(15)],
+        updateOn: 'blur'
+      },
+      []
+    ],
+    to: [
+      '',
+      {
+        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(15)],
+        updateOn: 'blur'
+      },
+      []
+    ],
+    date: ['', [Validators.required, Validators.minLength(33), Validators.maxLength(33)], []]
   });
 
   message = '';
